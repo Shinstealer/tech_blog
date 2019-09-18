dc82f9bc5afb9d2c11b336f0a828ba2801e3bc3d
sungmin <Shinstealer>
2019-09-18 23:34:20 +0900

    20190918-DB connection test

---
+++ blog/tech_blog/src/main/java/com/shinstealer/sample/Hotel.java
@@ -0,0 +1,18 @@
+package com.shinstealer.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Component
+@ToString
+@Getter
+@AllArgsConstructor // 积己磊 积己 绢畴抛捞记
+public class Hotel {
+
+	private Chef chef;
+	
+	
+}
