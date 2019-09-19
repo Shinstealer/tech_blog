24bc5ffaddd3df26162b0a1e8f361a817216d657
sungmin <Shinstealer>
2019-09-19 23:26:59 +0900

    Spring Connection Pool setting ( HikariCP) by JAVA and completed connection tests

--- blog/tech_blog/src/main/java/com/shinstealer/config/RootConfig.java
+++ blog/tech_blog/src/main/java/com/shinstealer/config/RootConfig.java
@@ -1,10 +1,30 @@
 package com.shinstealer.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 @Configuration
-@ComponentScan(basePackages = {"com.shinstealer.sample"})
+@ComponentScan(basePackages = { "com.shinstealer.sample" })
 public class RootConfig {
 
+	@Bean
+	public DataSource dataSource() {
+
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		hikariConfig.setUsername("blog");
+		hikariConfig.setPassword("1234");
+
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
+		return dataSource;
+
+	}
 }
